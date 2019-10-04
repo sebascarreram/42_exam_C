ff69c35fd0d284533a9f161530e8a3b6983687f7
Sebastian Carrera <sebascarreram@hotmail.com>
2019-10-04 16:07:23 -0500

    done

---
+++ 02/2-0-ft_atoi/ft_atoi.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int ft_atoi(const char *str)
+{
+	int index;
+	int sign;
+	long result;
+
+	sign = 1;
+	result = 0;
+	index = 0;
+	while (str[index])
+	{
+		if (str[index] == 32 || (str[index] >= 9 && str[index] <= 13))
+			index++;
+		if (str[index] == '-' && (str[index + 1] >= '0' && str[index + 1] <= '9'))
+		{
+			sign = -1;
+			index++;
+		}
+		while (str[index] >= '0' && str[index] <= '9')
+		{
+			result = (result * 10) + str[index] - 48;
+			index++;
+		}
+		index++;
+	}
+	return (sign * (int)result);
+}
+
+int main()
+{
+	char a[] = " + -  - \n\n\v\f\r\t 5234AAAgreg+hr&9sth"; // 52349
+	printf("%d\n", ft_atoi(a));
+	return (0);
+}
